164d2b808053ff54b431d3a46e5bd554d46843f6
Luiz Henrique <luizhenrique.se@gmail.com>
2023-08-22 20:56:17 -0300

    fix: httpservice post method

--- src/transactions/transactions.service.spec.ts
+++ src/transactions/transactions.service.spec.ts
@@ -1,20 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TransactionsService } from './transactions.service';
-import { HttpModule } from '@nestjs/axios';
+import { HttpService } from '@nestjs/axios';
+import { of } from 'rxjs';
 
 describe('TransactionsService', () => {
   let service: TransactionsService;
+  let httpService: HttpService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TransactionsService],
-      imports: [HttpModule],
+      providers: [
+        TransactionsService,
+        {
+          provide: HttpService,
+          useValue: {
+            post: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<TransactionsService>(TransactionsService);
+    httpService = module.get<HttpService>(HttpService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(httpService).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a random transaction with success', async () => {
+      jest
+        .spyOn(httpService, 'post')
+        .mockReturnValueOnce(
+          of({
+            status: 200,
+            statusText: 'OK',
+            config: {},
+            headers: {},
+            data: '',
+          }),
+        );
+      const result = await service.create();
+      expect(result).toBeTruthy();
+      expect(httpService.post).toBeCalledTimes(1);
+    });
   });
 });
--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -1,14 +1,15 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { HttpService } from '@nestjs/axios';
 import { v4 as uuidv4 } from 'uuid';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class TransactionsService {
   private readonly logger = new Logger(TransactionsService.name);
   constructor(private readonly httpService: HttpService) {}
 
-  create() {
+  async create(): Promise<boolean> {
     const transaction: CreateTransactionDto = {
       id: uuidv4(),
       client_document: uuidv4(),
@@ -20,9 +21,17 @@ export class TransactionsService {
       `Calling bank service with transactionId : ${transaction.id}`,
     );
 
-    return this.httpService.post(
-      'https://webhook.site/e7648810-8661-47af-8b64-cff6e4b0e630',
-      transaction,
+    const url = 'https://webhook.site/e7648810-8661-47af-8b64-cff6e4b0e630';
+    const config = {
+      headers: {
+        'Content-Type': 'Application/json',
+      },
+    };
+
+    const response = await lastValueFrom(
+      this.httpService.post(url, transaction, config),
     );
+
+    return response.status == HttpStatus.OK;
   }
 }
